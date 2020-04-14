c2d5462be5850cb5d9f222e6d3a19f813dccfb22
Francois Colombo <francois_colombo@yahoo.fr>
2020-04-14 19:32:00 +0200

    fix issue with polices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { OwlModule } from 'ngx-owl-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +28,6 @@ import { FcExperienceService } from './fc-experiences/fc-experiences.service';
   ],
   imports: [
     BrowserModule,
-    OwlModule,
     HttpClientModule,
     AppRoutingModule
   ],
